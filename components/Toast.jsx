89edc054a14ca6da344fb637c860cd3de9cc8171
Michael Kilong <michaelkilongz@gmail.com>
2026-06-04 08:10:43 +0530

    Add Toast component to display notifications

---
+++ components/Toast.jsx
@@ -0,0 +1,15 @@
+'use client';
+
+export default function Toast({ toasts }) {
+  if (!toasts.length) return null;
+  return (
+    <div className="toast-stack">
+      {toasts.map((t) => (
+        <div key={t.id} className={`toast toast-${t.type}`}>
+          {t.message}
+        </div>
+      ))}
+    </div>
+  );
+}
+
