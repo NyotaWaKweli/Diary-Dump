0b6983c9c4bc8021b7c095a485bc20916dd9b358
NyotaWaKweli <nurubenjamin0001@icloud.com>
2026-06-05 06:45:19 +0300

    Delete next.config.js
    Delete

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['firebase-admin', 'bcryptjs'],
-  },
-};
-module.exports = nextConfig;
-
